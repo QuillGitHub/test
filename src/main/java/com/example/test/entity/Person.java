52d9985f91b34b86e4be623d9f51dab107ce27db
zhihesi <2302521258@qq.com>
2018-05-09 23:53:29 +0800

    get set

--- src/main/java/com/example/test/entity/Person.java
+++ src/main/java/com/example/test/entity/Person.java
@@ -13,6 +13,28 @@ public class Person {
 
     private Integer age;
 
+    public String getFullName() {
+        return fullName;
+    }
 
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
 
